4c228493c91c2f725f82e7177782efd0b15d29ac
zox1r <qarshiyevzoxir@gmail.com>
2023-07-06 12:54:21 +0200

    bin/basic_func09.dart

--- bin/basic_func09.dart
+++ bin/basic_func09.dart
@@ -3,4 +3,11 @@
 //  Create function with parameter a type of int.
 
 //  decrease the value of a to one and return
-void main() {}
+int func(int son){
+    int a = son - 1;
+    return a;
+}
+void main() {
+    int a = func(5);
+    print(a);
+}
